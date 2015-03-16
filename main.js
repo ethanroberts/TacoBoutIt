340ddb9785542b99000dbcd92208e060738eebed
Guest User <Guest@f-06-06-lab.mddn.vuw.ac.nz>
2015-03-17 11:28:13 +1300

    Commented out code

--- main.js
+++ main.js
@@ -3,21 +3,25 @@
 	var numOfSeconds = document.getElementById ("seconds");
 	var seconds = 0, tortillas = 0, step = 2695;
 
-function startCount () {
+function startCount () { //start tortilla counter
 	var tortillaCounter = setInterval(animTortillas, 1000);
 }
 
 function animTortillas () {
-	//numOfTortillas.innerHTML.animateNumber({number: 165});
-	//numOfSeconds.innerHTML.animateNumber({number: 1456});
+	//check innerHTML of numOfTortillas
+		//console.log("# of Tortillas " + numOfTortillas.innerHTML);
+
+	//setup comma separator
 	var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+
 	$('#seconds')
 		.prop('number', seconds)
 		.animateNumber({ number: seconds+1, numberStep: comma_separator_number_step});
 	$('#tortillasConsumed')
 		.prop('number', tortillas)
 		.animateNumber({ number: tortillas+step, numberStep: comma_separator_number_step});
-
+	
+	//Increment seconds and tortillas
 	seconds++;
 	tortillas += step;
 }
